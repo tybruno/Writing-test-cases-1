73b9164cf79d390ae4c5491be2c2d20c7f5a64b0
Tyler <tyler.bruno@sjsu.edu>
2018-02-03 20:34:52 -0800

    created test cases

---
+++ Project/src/Triangle.java
@@ -0,0 +1,67 @@
+public class Triangle {
+
+    private int x;
+    private int y;
+    private int z;
+
+    public Triangle(int x,int y,int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void getValues(){
+        System.out.println("X: " + x);
+        System.out.println("X: " + y);
+        System.out.println("X: " + z);
+
+    }
+    public boolean checkEquilateral(){
+
+        //if x implies y then y implies z
+        if(x == y && y == z && x > 0)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean checkIsosceles(){
+
+        if(x == y)
+        {
+            if(x > z)
+            {
+               return true;
+            }
+        }
+        if (y == z){
+            if(y > z){
+                return true;
+            }
+
+        }
+        if(x == z)
+        {
+            if(x > y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    public boolean checkScalene()
+    {
+        if(x != y && y != z)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+}
--- Project/src/main.java
+++ Project/src/main.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //assume all values entered are greater than 0
+        Triangle equilateral = new Triangle(10,10,10);
+        Triangle scalene = new Triangle(2,10,20);
+        Triangle isoscelese = new Triangle(5,5,3);
+
+        ArrayList triangles = new ArrayList();
+        triangles.add(equilateral);
+        triangles.add(scalene);
+        triangles.add(isoscelese);
+
+
+//        for(int i = 0;i < triangles.size();i++)
+//        {
+//            triangles.get(i). ;
+//
+//            System.out.println(t.checkEquilateral());
+////            System.out.println(triangles.get(i).checkEquilateral());
+////            System.out.println(triangles.get(i).checkscalene());
+////            System.out.println(triangles.get(i).checkisoscelese());
+//
+//        }
+
     }
+
+
 }
 
 
